6b37fe20671f5a8407c57a5b14bf06b58f94c59d
h.kato <h.kato@cocolabo.jp>
2021-03-12 14:03:01 +0900

    aws sms sens testing command

---
+++ app/Console/Commands/SendAwsSnsSms.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+
+class SendAwsSnsSms extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:sns';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $sns = App::make('aws')->createClient('sns');
+
+        $sns->publish([
+            'Message' => "こんにちは\nアイウエオ",
+            'PhoneNumber' => '+81xxxxxxxx',
+            'MessageAttributes' => [
+                'AWS.SNS.SMS.SenderID' => [
+                    'DataType'    => 'String',
+                    'StringValue' => 'Aiseki'
+                ]
+            ]
+        ]);
+
+        $message = 'This message is sent from a Amazon SNS code sample.';
+        $topic = 'arn:〜〜〜';
+
+
+//        $sns->subscribe([
+//            'Protocol'              => 'sms',
+//            'Endpoint'              => '+818010551397',
+////            'Endpoint' => '+818030185174',
+//            'ReturnSubscriptionArn' => true,
+//            'TopicArn'              => $topic,
+//        ]);
+
+//        $sns->publish([
+//            'Message'  => $message,
+//            'TopicArn' => $topic,
+//        ]);
+    }
+}
